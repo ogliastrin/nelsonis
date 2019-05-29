19753d81aaa7220966aedb7de62745c57f54dd7a
Nelson Ogliastri <Ogliastri@nelsons-mbp.home>
2019-05-29 14:38:52 +0200

    Complete assignment.

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'gE6YVCFfdj6hA4xTnTagpthXnADh56OehFcph1576Ro9gc0zJU6iTbrnfxq7u0IHJNpGo8zZC4Up3ASsgFI_nknmdIzdDthP_HDvgyU-BmABDaoIT6fWm9tZiVfuXHYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
